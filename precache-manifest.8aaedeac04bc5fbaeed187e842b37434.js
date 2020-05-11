1ba72b1d543c3766f760bb1324f53d5cc71970ff
Mohamed Ziata <wakematta@gmail.com>
2020-05-11 19:29:07 +0200

    Updates

---
+++ precache-manifest.8aaedeac04bc5fbaeed187e842b37434.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ff28c6f76886c9ae62628e04321a62",
+    "url": "/sefart/index.html"
+  },
+  {
+    "revision": "c7cc208eeb71ed53308b",
+    "url": "/sefart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d5778acf4a33d76b8ab",
+    "url": "/sefart/static/js/2.ec27bf25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sefart/static/js/2.ec27bf25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7cc208eeb71ed53308b",
+    "url": "/sefart/static/js/main.d7989e3d.chunk.js"
+  },
+  {
+    "revision": "bccb4e34efff8fea1406",
+    "url": "/sefart/static/js/runtime-main.7bfd24e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sefart/static/media/logo.5d5d9eef.svg"
+  }
+]);
